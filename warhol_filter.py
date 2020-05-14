bd9d5bb1f22762e4b918b8f329a344e02df5458f
lucasflosi <lucasflosi@gmail.com>
2020-05-14 00:13:32 -0700

    second push maybe?

--- warhol_filter.py
+++ warhol_filter.py
@@ -5,41 +5,31 @@ This program generates the Warhol effect based on the original image.
 from simpleimage import SimpleImage
 import random
 
-N_ROWS = 4
-N_COLS = 5
+N_ROWS = 2 #must be greater than 1 .. I could have user input here
+N_COLS = 2 #must be greater than 1.. I could have user input here
 PATCH_SIZE = 222
 WIDTH = N_COLS * PATCH_SIZE
 HEIGHT = N_ROWS * PATCH_SIZE
+LOW_RANDOM_RANGE = 0
+HIGH_RANDOM_RANGE = 199 #199 is selected because since we divide by 1 it results in 0 to 2X the value
+
+#Various Patches below here. Could create a user input, but prefer to leave it hardcoded for now
+
 #PATCH_NAME = 'images/luval4.jpg'
 PATCH_NAME = 'images/simba-sq.jpg'
 #PATCH_NAME = 'images/stanford.png'
 #PATCH_NAME = 'images/Blue.jpg'
-LOW_RANDOM_RANGE = 0
-HIGH_RANDOM_RANGE = 199 #199 is selected because since we divide by 1 it results in 0 to 2X the value
 
 def main():
     patch = SimpleImage(PATCH_NAME)
+    #imports an image, names it "patch"
     final_image = SimpleImage.blank((patch.width*N_COLS),(patch.height*N_ROWS))
     #generates blank canvas
     final_image = grid_of_patches(patch,final_image)
     #runs script that makes a grid of patches
     final_image.show()
-    #print(str(final_image.width))
-    #print(str(final_image.height))
 
-"""
-def grid_of_patches(patch,final_image):
-    image_height = patch.height
-    image_width = patch.width
-    for cols in range(N_COLS):
-        for rows in range (N_ROWS):
-            patch = random_recolored_patch()
-            for y in range(patch.height):
-                for x in range(patch.width):
-                    pixel = patch.get_pixel(x, y)
-                    final_image.set_pixel(x + (cols * image_width), y + (rows * image_height), pixel)
-    return final_image
-    """
+#creates rows and columns of an image patch
 def grid_of_patches(patch,final_image):
     for cols in range(N_COLS):
         for rows in range(N_ROWS):
@@ -47,7 +37,7 @@ def grid_of_patches(patch,final_image):
             final_image = single_patch(final_image, patch, cols, rows)
     return final_image
 
-
+#Used in <grid of patches> function above. Creates a single patch
 def single_patch(final_image, patch, cols, rows):
     for y in range(patch.height):
         for x in range(patch.width):
@@ -56,18 +46,8 @@ def single_patch(final_image, patch, cols, rows):
             pixel = patch.get_pixel(x, y)
             final_image.set_pixel(x + (cols * image_width), y + (rows * image_height), pixel)
     return final_image
-"""
-def row_of_patches(patch,final_image):
-    for i in range(N_COLS):
-        patch = random_recolored_patch()
-        for y in range(patch.height):
-            for x in range(patch.width):
-                pixel = patch.get_pixel(x,y)
-                final_image.set_pixel(x+(i*PATCH_SIZE), y, pixel)
-    return final_image
 
-"""
-
+#generates a randomly filtered image patch
 def random_recolored_patch():
     patch = SimpleImage(PATCH_NAME)
     red_scale = (random.randint(LOW_RANDOM_RANGE, HIGH_RANDOM_RANGE) + random.randint(LOW_RANDOM_RANGE, HIGH_RANDOM_RANGE)) / 200
